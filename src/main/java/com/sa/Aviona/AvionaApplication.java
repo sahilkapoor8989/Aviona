2a85b6d039e4106cc2137b057c3ffc3b6527dead
Sahil Kapoor <sahilkapoor@technogises-MacBook-Pro.local>
2025-04-10 23:36:25 +0530

    Intial commit- Springboot setup

---
+++ src/main/java/com/sa/Aviona/AvionaApplication.java
@@ -0,0 +1,13 @@
+package com.sa.Aviona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvionaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvionaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sa/Aviona/AvionaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sa.Aviona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvionaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
